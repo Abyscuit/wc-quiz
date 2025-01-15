a7b2a8b3a3b4684a4b3ee3b81e18b85c0bad4eaf
Abyscuit <36307949+Abyscuit@users.noreply.github.com>
2025-01-14 21:05:08 -0800

    devop: refactor founder to quiz template

--- app/founder/[[...routes]]/route.tsx
+++ app/founder/[[...routes]]/route.tsx
@@ -1,18 +1,18 @@
 /** @jsxImportSource frog/jsx */
 
-import { Button } from 'frog';
 import { devtools } from 'frog/dev';
 // import { neynar } from 'frog/hubs'
 import { handle } from 'frog/next';
 import { serveStatic } from 'frog/serve-static';
-import { bg, container, fontStyle } from '@/app/styles/styles';
-import { createApp } from '@/app/template/quiz';
+import {
+  createApp,
+  createIntro,
+  createQuestionPage,
+  createResultPage,
+} from '@/app/template/quiz';
 
 const title = 'Which crypto founder are you?';
 const app = createApp('/founder', title);
-
-const enkryptLink =
-  'https://chrome.google.com/webstore/detail/enkrypt/kkpllkodjeloidieedojogacfhpaihoh';
 
 const questions = [
   {
@@ -62,152 +62,51 @@ const founders = [
     desc: "It's giving we could be cellies.",
     enkryptDesc:
       'And he/you should have used a self custody wallet like Enkrypt!',
+    img: `/images/founders/Sam-Bankman-Fried.png`,
   },
   {
     name: 'Do Kwon',
     desc: 'Not afraid to try new things!',
     enkryptDesc:
       'And he/you should have used a self custody wallet like Enkrypt!',
+    img: `/images/founders/Do-Kwan.png`,
   },
   {
     name: 'CZ',
     desc: "You're quick to act and overcome any challenges.",
     enkryptDesc:
       "And he/you'd use a self custody multichain wallet like Enkrypt!",
+    img: `/images/founders/CZ.png`,
   },
   {
     name: 'Vitalik Buterin',
     desc: "You used to play WoW, now you're an onchain hero and animal lover.",
     enkryptDesc:
       "And he/you'd use a self custody multichain wallet like Enkrypt!",
+    img: `/images/founders/Vitalik-Buterin.png`,
   },
   {
     name: 'Satoshi Nakamoto',
     desc: 'Very calculated. Very based.',
     enkryptDesc:
       "And he/you'd use a self custody multichain wallet like Enkrypt!",
+    img: `/images/founders/Satoshi-Nakamoto.png`,
   },
 ];
 
-const storedAnswers: number[] = [];
-let questionNum = 0;
-
 app.frame('/', c => {
-  return c.res({
-    image: (
-      <div style={container}>
-        <img
-          alt='background'
-          src='/images/founder-background.png'
-          width={'100%'}
-          height={'100%'}
-          style={bg}
-        />
-        <div style={fontStyle}>{app.title}</div>
-      </div>
-    ),
-    intents: [
-      <Button value='reset' action='/questions'>
-        Let's find out!
-      </Button>,
-    ],
-  });
+  return c.res(createIntro(title, '/images/founder-background.png'));
 });
-
-function incrementQuestion(value: string) {
-  questionNum++;
-  if (value !== 'reset') storedAnswers.push(parseInt(value));
-}
 
 app.frame('/questions', c => {
   const { buttonValue } = c;
-  if (buttonValue === 'reset') {
-    storedAnswers.splice(0, storedAnswers.length);
-    questionNum = 0;
-  }
-  const lastQuestion = questionNum === questions.length - 1;
-  const linkAction = lastQuestion ? '/result' : '';
-  const currentQuestion = questions[questionNum];
-  return c.res({
-    image: (
-      <div style={container}>
-        <img
-          alt='background'
-          src='/background.png'
-          width={'100%'}
-          height={'100%'}
-          style={bg}
-        />
-        {incrementQuestion(buttonValue ?? '')}
-        <div style={{ ...fontStyle, textShadow: '0px 0px' }}>
-          {currentQuestion.question}
-        </div>
-      </div>
-    ),
-    intents: [
-      ...currentQuestion.answers.map(answer => {
-        return (
-          <Button value={answer.weight} action={linkAction}>
-            {answer.answer}
-          </Button>
-        );
-      }),
-      <Button.Reset>Start Over</Button.Reset>,
-    ],
-  });
+  return c.res(createQuestionPage(buttonValue, questions));
 });
 
 app.frame('/result', c => {
   const { buttonValue } = c;
-  if (buttonValue && !isNaN(parseInt(buttonValue))) {
-    storedAnswers.push(parseInt(buttonValue));
-  }
-  const result = calculateResult();
-  const founder = founders[result];
-  const founderImg = `/images/founders/${founder.name.replace(' ', '-')}.png`;
-  return c.res({
-    image: (
-      <div style={{ ...container, flexDirection: 'row' }}>
-        <img
-          alt='background'
-          src='/background.png'
-          width={'100%'}
-          height={'100%'}
-          style={bg}
-        />
-        <img src={founderImg} width={380} height={380} alt={founder.name} />
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignText: 'center',
-            alignItems: 'center',
-            justifyContent: 'center',
-            width: '60%',
-          }}
-        >
-          <div
-            style={{ ...fontStyle, fontSize: 45, textShadow: '0px 0px' }}
-          >{`You are ${founder.name}.\n${founder.desc}\n${founder.enkryptDesc}`}</div>
-        </div>
-      </div>
-    ),
-    intents: [
-      <Button.Link href={enkryptLink}>Download Enkrypt</Button.Link>,
-      <Button.Reset>Start Over</Button.Reset>,
-    ],
-  });
+  return c.res(createResultPage(buttonValue, founders));
 });
-
-function calculateResult(): number {
-  let sum = 0;
-  for (const element of storedAnswers) {
-    sum += element;
-  }
-  sum = Math.floor(sum / 2);
-  if (sum > founders.length - 1) sum = founders.length - 1;
-  return sum;
-}
 
 devtools(app, { serveStatic });
 
