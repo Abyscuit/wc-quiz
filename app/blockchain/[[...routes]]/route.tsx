0d300728dddee8f770ec5c24891547dd1141c2f1
Abyscuit <36307949+Abyscuit@users.noreply.github.com>
2025-01-14 21:23:17 -0800

    feat: add blockchain quiz

---
+++ app/blockchain/[[...routes]]/route.tsx
@@ -0,0 +1,129 @@
+/** @jsxImportSource frog/jsx */
+
+import { devtools } from 'frog/dev';
+// import { neynar } from 'frog/hubs'
+import { handle } from 'frog/next';
+import { serveStatic } from 'frog/serve-static';
+import {
+  createApp,
+  createIntro,
+  createQuestionPage,
+  createResultPage,
+} from '@/app/template/quiz';
+import { Questions, Results } from '@/app/template';
+
+const title = 'What blockchain are you?';
+const app = createApp('/blockchain', title);
+
+const questions: Questions = [
+  {
+    question: 'On chain, on-chain or onchain?',
+    answers: [
+      { answer: 'On Chain', weight: '0' },
+      { answer: 'On-Chain', weight: '1' },
+      { answer: 'Onchain', weight: '2' },
+    ],
+  },
+  {
+    question: 'Memecoins or DeFi?',
+    answers: [
+      { answer: 'Memecoins', weight: '0' },
+      { answer: 'DeFi', weight: '1' },
+      { answer: '1) What', weight: '2' },
+    ],
+  },
+  {
+    question: "I'm in it for the…",
+    answers: [
+      { answer: 'Memes', weight: '0' },
+      { answer: 'Money', weight: '1' },
+      { answer: 'Tech', weight: '2' },
+    ],
+  },
+  {
+    question: 'When did you start in crypto?',
+    answers: [
+      { answer: 'Post 2020', weight: '0' },
+      { answer: '2017-2020', weight: '1' },
+      { answer: 'Pre 2017', weight: '2' },
+    ],
+  },
+  {
+    question: 'Are you currently staking any crypto?',
+    answers: [
+      { answer: 'Maybe?', weight: '0' },
+      { answer: 'No', weight: '1' },
+      { answer: 'Yes', weight: '2' },
+    ],
+  },
+];
+const crypto: Results = [
+  {
+    name: 'Dogecoin',
+    desc: "It's giving fun to be around.",
+    enkryptDesc:
+      'Easily manage your Dogecoin with our multichain browser wallet Enkrypt!',
+    img: '/images/crypto/Dogecoin.png',
+  },
+  {
+    name: 'Solana',
+    desc: 'Not afraid to embrace your silly side!',
+    enkryptDesc:
+      'Easily manage your Solana with our multichain browser wallet Enkrypt!',
+    img: '/images/crypto/Solana.png',
+  },
+  {
+    name: 'Ethereum',
+    desc: 'You adapt to challenges and are not afraid to try new things.',
+    enkryptDesc:
+      'Easily manage your Ethereum with our multichain browser wallet Enkrypt!',
+    img: '/images/crypto/Ethereum.png',
+  },
+  {
+    name: 'Bitcoin',
+    desc: 'A reliable leader, the people love you!',
+    enkryptDesc:
+      'Easily manage your Bitcoin with our multichain browser wallet Enkrypt!',
+    img: '/images/crypto/Bitcoin.png',
+  },
+  {
+    name: 'Base',
+    desc: 'Basically very based.',
+    enkryptDesc:
+      'Easily manage your Ethereum on Base with our multichain browser wallet Enkrypt!',
+    img: '/images/crypto/Base.png',
+  },
+];
+
+app.frame('/', c => {
+  return c.res(createIntro(title));
+});
+
+app.frame('/questions', c => {
+  const { buttonValue } = c;
+  return c.res(createQuestionPage(buttonValue, questions));
+});
+
+app.frame('/result', c => {
+  const { buttonValue } = c;
+  return c.res(createResultPage(buttonValue, crypto));
+});
+
+devtools(app, { serveStatic });
+
+export const GET = handle(app);
+export const POST = handle(app);
+
+// NOTE: That if you are using the devtools and enable Edge Runtime, you will need to copy the devtools
+// static assets to the public folder. You can do this by adding a script to your package.json:
+// ```json
+// {
+//   scripts: {
+//     "copy-static": "cp -r ./node_modules/frog/_lib/ui/.frog ./public/.frog"
+//   }
+// }
+// ```
+// Next, you'll want to set up the devtools to use the correct assets path:
+// ```ts
+// devtools(app, { assetsPath: '/.frog' })
+// ```
