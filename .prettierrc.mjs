aa5fab3a69091923421a9ec05181dd7f4d07e6bf
Abyscuit <36307949+Abyscuit@users.noreply.github.com>
2025-02-19 16:07:23 -0800

    chore: 🔧 install prettier and husky

---
+++ .prettierrc.mjs
@@ -0,0 +1,14 @@
+const prettierConfig = {
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  jsxSingleQuote: true,
+  singleAttributePerLine: true,
+  printWidth: 80,
+};
+
+export default prettierConfig;
